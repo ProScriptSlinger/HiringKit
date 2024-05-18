4bd782774ba6ad0244ef98d68b73145f4ae3848e
ProScriptSlinger <brondon.lee.dr@gmail.com>
2024-05-19 00:25:50 +0900

    fix: file name

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 const Logo = () => {
-  return <Image src="/images/Logo4.PNG" alt="logo" width={50} height={50} />;
+  return <Image src="/images/Logo4.png" alt="logo" width={50} height={50} />;
 };
 export default Logo;

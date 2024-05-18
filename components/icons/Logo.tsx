8628381ccb8935d93bbab668309bcb90ed03496b
ProScriptSlinger <brondon.lee.dr@gmail.com>
2024-05-18 23:27:05 +0900

    fix: Image optimisation

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,4 +1,5 @@
+import Image from "next/image";
 const Logo = () => {
-  return <img src="/images/Logo4.PNG" width="50px" height="50px" />;
+  return <Image src="/images/Logo4.PNG" alt="logo" width={50} height={50} />;
 };
 export default Logo;

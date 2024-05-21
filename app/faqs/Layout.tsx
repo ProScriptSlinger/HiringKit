52627ec4e7f0305544d9962932898e04aacf3fd1
ProScriptSlinger <brondon.lee.dr@gmail.com>
2024-05-22 02:12:51 +0900

    fix: faq header issue

--- app/faqs/Layout.tsx
+++ app/faqs/Layout.tsx
@@ -1,14 +1,7 @@
-import Breadcrumb from "@/components/Breadcrumb";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="flex flex-col gap-2 bg-[#FCFCFC]">
-      <Breadcrumb path="Faqs" />
-      {children}
-    </div>
-  );
+  return <div className="flex flex-col gap-2 bg-[#FCFCFC]">{children}</div>;
 }
--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -3,24 +3,28 @@
 import { useState } from "react";
 import CollapseItem from "./CollapseItem";
 import { data } from "./data";
+import Breadcrumb from "@/components/Breadcrumb";
 
 const Faqs = () => {
   const [activeId, setActiveId] = useState<number>(-1);
   return (
-    <div className="flex flex-col px-2 sm:px-8 md:px-16 transition-all pb-8">
-      <div className="text-[25px] pb-2 md:text-[30px] lg:text-[40px] font-title text-center">
-        {`FAQ's`}
+    <>
+      <Breadcrumb path="Faqs" />
+      <div className="flex flex-col px-2 sm:px-8 md:px-16 transition-all pb-8">
+        <div className="text-[25px] pb-2 md:text-[30px] lg:text-[40px] font-title text-center">
+          {`FAQ's`}
+        </div>
+        {data.map((item, index) => (
+          <CollapseItem
+            index={index}
+            key={index}
+            {...item}
+            activeId={activeId}
+            setActiveId={setActiveId}
+          />
+        ))}
       </div>
-      {data.map((item, index) => (
-        <CollapseItem
-          index={index}
-          key={index}
-          {...item}
-          activeId={activeId}
-          setActiveId={setActiveId}
-        />
-      ))}
-    </div>
+    </>
   );
 };
 

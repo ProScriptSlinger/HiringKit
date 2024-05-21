e558960fb1ee586f236c6c56f29ab619b43a6c66
ProScriptSlinger <brondon.lee.dr@gmail.com>
2024-05-22 02:07:30 +0900

    fix: faqs breadcrumb issue

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -7,13 +7,7 @@ import { data } from "./data";
 const Faqs = () => {
   const [activeId, setActiveId] = useState<number>(-1);
   return (
-    <div className="flex flex-col px-2 sm:px-8 md:px-16 transition-all bg-[#FCFCFC] pb-8">
-      {/* <Link href="/">
-        <FaHome
-          className="my-8 text-black/70 cursor-pointer hover:text-[#1C64F2]/70"
-          size={30}
-        />
-      </Link> */}
+    <div className="flex flex-col px-2 sm:px-8 md:px-16 transition-all pb-8">
       <div className="text-[25px] pb-2 md:text-[30px] lg:text-[40px] font-title text-center">
         {`FAQ's`}
       </div>
